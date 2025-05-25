0ed88f44689cba3266631df0763c44af7fb231e7
Sunil1718 <Arnil1718@gmail.com>
2025-05-25 12:52:30 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -6,7 +6,7 @@ class WZMLStyle:
     ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
     ST_BN2_NAME = 'Updates'
     ST_BN2_URL = 'https://t.me/SS_Bot18'
-    ST_MSG = '''<i>This bot an mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
+    ST_MSG = '''<i>This bot can Leech Videos Or Documnets Files From your ddl-links | files | torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>'''
